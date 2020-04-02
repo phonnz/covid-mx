bd3d8221b87fc1f1bb44b7ce9263b4e40ddcb8b5
phonnz <gonzalezm.alfonso@gmail.com>
2020-04-02 18:53:37 -0300

    Fix domian Y range

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -3,11 +3,6 @@ import { ResponsiveContainer, ReferenceLine, LineChart, Legend, Line, CartesianG
 // import {  } from 'semantic-ui-react'
 import Mexico from './Flags/Mexico';
 
-const CustomizedLabel = props => {
-  const {x, y, stroke, value} = props;
-  
-  return <text x={x} y={y} dy={-4} fill={stroke} fontSize={10} textAnchor="middle" fill="white">{value}</text>
-}
 
 class growChart extends Component {
   state = {
@@ -17,15 +12,7 @@ class growChart extends Component {
   }
 
 
-  getRandomColor = (country) => {
-    if(country.key === "Mexico") return '#de2d1b'
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
+  
 
 
   render() {
@@ -34,21 +21,21 @@ class growChart extends Component {
       <ResponsiveContainer height={400}>
         <LineChart width={500} height={450} data={this.props.data} margin={{ top: 5, right: 5, bottom: 5, left: -15 }}>
           <XAxis dataKey="name" />
-          <YAxis  type="number" label={{ value: 'Fallecidos', angle: 90, position: 'insideLeft', fill: 'white' }} domain={['auto', 'auto']} />
-          <ReferenceLine x='3/22/20' stroke="red" label={<Label value="F-II-MX" fill={'white'} textAnchor={'start'} /> }  />
-          <ReferenceLine x='3/17/20' stroke="red" label={<Label value="F-I-AR" fill={'white'} textAnchor={'start'} /> }  />
-          <ReferenceLine x='3/20/20' stroke="red" label={<Label value="F-II-AR" fill={'white'} textAnchor={'middle'} /> }  />
+          <YAxis  type="number" label={{ value: 'Fallecidos', angle: 90, position: 'insideLeft', fill: 'white' }} domain={['auto', dataMax => (this.props.max*1.3) ]} />
+          <ReferenceLine x='3/22/20' stroke="red" label={<Label value="F-II-MX" fill={'red'} position={'insideTopLeft'} /> }  />
+          <ReferenceLine x='3/17/20' stroke="red" label={<Label value="F-I-AR" fill={'red'} position={'insideTopLeft'} /> }  />
+          <ReferenceLine x='3/20/20' stroke="red" label={<Label value="F-II-AR" fill={'red'} position={'insideTopLeft'} /> }  />
           
           <CartesianGrid strokeDasharray="3 3" />
           <Tooltip itemStyle={{  backgroundColor: '#363738' }} contentStyle={{backgroundColor: '#363738'}} wrapperStyle={{ backgroundColor: '#363738', border: '1px solid #fff', borderRadius: 3 }} />
           <Legend  wrapperStyle={{ left: '10', backgroundColor: '#363738', border: '1px solid #d5d5d5', borderRadius: 3 }}/>
           {this.props.countries.map((country, idx) => {
               if(country.key === "Mexico"){
-                return (<Line key={idx} type="monotone" dataKey={country.key} stroke={this.getRandomColor(country)} strokeWidth={3}  dot={<Mexico />} />)
+                return (<Line key={idx} type="monotone" dataKey={country.key} stroke={country.color} strokeWidth={3}  dot={<Mexico />} />)
                 
               }  else {
                 
-                return (<Line key={idx} type="monotone" dataKey={country.key }  stroke={this.getRandomColor(country)} strokeWidth={1} label={country.name} dot={false} /> )
+                return (<Line key={idx} type="monotone" dataKey={country.key }  stroke={country.color} strokeWidth={1} label={country.name} dot={false} /> )
               }
 
           })}
