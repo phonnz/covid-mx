c0d2c399809c5cd9749f4382b95de335f9954695
phonnz <gonzalezm.alfonso@gmail.com>
2020-04-13 03:18:14 -0300

    Adding Portugal Flag

--- src/Components/Flags/Flag.js
+++ src/Components/Flags/Flag.js
@@ -204,6 +204,17 @@ const Flag = props => {
                     </svg>
                 )
 
+            case 'Portugal':
+                return(
+                    <svg key={`${r}-${index}`} xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" x={cx - 10} y={cy - 10} width={30} height={35} viewBox="0 0 1024 1024" style={{ enableBackground: 'new 0 0 512 512' }} >
+                    <rect y="85.337" fill="#D80027" width="512" height="341.326"/>
+<polygon fill="#6DA544" points="196.641,85.337 196.641,261.565 196.641,426.663 0,426.663 0,85.337 "/>
+<circle fill="#FFDA44" cx="196.641" cy="256" r="64"/>
+<path fill="#D80027" d="M160.638,224v40.001c0,19.882,16.118,36,36,36s36-16.118,36-36V224H160.638z"/>
+<path fill="#F0F0F0" d="M196.638,276c-6.617,0-12-5.383-12-12v-16h24.001v16C208.638,270.616,203.254,276,196.638,276z"/><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
+</svg>
+                )
+
             case 'US':
                 return (
                     <svg key={`${r}-${index}`} xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" x={cx - 10} y={cy - 10} width={30} height={35} viewBox="0 0 1024 1024" style={{ enableBackground: 'new 0 0 512 512' }} >
