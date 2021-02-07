4dbd51f5dd012601acaa7cab8b5b38a57e7cb998
phonnz <gonzalezm.alfonso@gmail.com>
2021-02-06 23:13:13 -0600

    Drop info lines

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -18,9 +18,6 @@ class growChart extends Component {
         <LineChart width={500} height={450} data={this.props.data} margin={{ top: 5, right: 5, bottom: 5, left: -15 }}>
           <XAxis dataKey="name" />
           <YAxis  type="number" label={{ value: 'Fallecidos', angle: 90, position: 'insideLeft', fill: 'white' }} domain={['dataMin', dataMax => (this.props.max*1.05) ]} />
-          <ReferenceLine x='3/22/20' stroke="red" label={<Label value="F-II-MX" fill={'red'} position={'insideTopLeft'} /> }  />
-          <ReferenceLine x='3/17/20' stroke="red" label={<Label value="F-I-AR" fill={'red'} position={'insideTopLeft'} /> }  />
-          <ReferenceLine x='3/20/20' stroke="red" label={<Label value="F-II-AR" fill={'red'} position={'insideTopLeft'} /> }  />
           
           <CartesianGrid strokeDasharray="3 3" />
           <Tooltip itemStyle={{  backgroundColor: '#363738' }} contentStyle={{backgroundColor: '#363738'}} wrapperStyle={{ backgroundColor: '#363738', border: '1px solid #fff', borderRadius: 3 }} />
